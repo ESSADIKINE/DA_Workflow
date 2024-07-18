f633b84d05849f08003a0c03777bfd47cd998c81
ESSADIKINE <anasessadikine.fst@gmail.com>
2024-07-18 17:21:12 +0200

    implimentaion de demande d'achat

---
+++ backend/utils/templates/Email.js
@@ -0,0 +1,39 @@
+const nodemailer = require('nodemailer'); 
+const MailGen = require('mailgen'); 
+
+const sendMail = async (email, messageTemplate) => {
+    
+    // Set transporter configuration
+    const transporterConfig = {
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS,
+        },
+    }
+    const transporter = nodemailer.createTransport(transporterConfig);
+                    
+    // Create instance of MailGen
+    const MailGenerator = new MailGen({
+        theme: "default",
+        product: {
+            name: "DecayeuxSTM",
+            link: "https://mailgen.js/"
+        }
+    })
+
+    // Generate the email
+    const mail = MailGenerator.generate(messageTemplate)
+                    
+    // Set mail config
+    const mailConfig = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: 'DecayeuxSTM demande d\'achat',
+        html: mail
+    }
+    // Send the email
+    await transporter.sendMail(mailConfig);
+}
+
+module.exports = { sendMail }
